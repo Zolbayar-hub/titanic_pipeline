d2b200c85f5b0f869e8644e0482618a573cd0702
Zolbayar-hub <Zolbayar-hub>
2024-10-29 23:43:33 +0000

    added

---
+++ Survived.sql
@@ -0,0 +1,17 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC import pandas as pd
+-- MAGIC from pyspark.sql import functions as F
+
+-- COMMAND ----------
+
+select PassengerID, Name, Sex, Survived from titanic where Survived = 1
+
+-- COMMAND ----------
+
+SELECT SUM(Survived) AS number_of_survivors FROM hive_metastore.default.titanic
+
+-- COMMAND ----------
+
+create table if not exists survivors as
+select PassengerId, Name, Sex, Survived from titanic where Survived = 1
