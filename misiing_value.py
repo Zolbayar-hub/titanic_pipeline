5f086076170096eee7dbcd26a24f8abcb4927531
Zolbayar-hub <Zolbayar-hub>
2024-11-17 16:55:12 +0000

    commit

--- misiing_value.py
+++ misiing_value.py
@@ -25,12 +25,4 @@ display(df)
 
 # COMMAND ----------
 
-df = (
-  spark.sql("SELECT * FROM titanic")
-  .fillna({"Age": 0})
-  .select("PassengerId", "Name", col)
-  .withColumnRenamed("PassengerId", "Id")
-  .withColumnRenamed("Name", "name")
-  .filter(F.col(col)>float(filter))
-  )
-display(df)
+
